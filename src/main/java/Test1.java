c454dc4345ddb1a8599826b53c310faf4d00b84c
poorva <poorva@hp>
2026-03-15 09:32:55 +0530

    First commit

---
+++ src/main/java/Test1.java
@@ -0,0 +1,13 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriver d=new ChromeDriver();
+		d.get("https://github.com/");
+	}
+
+}
